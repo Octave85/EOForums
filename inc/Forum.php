794169d640a7f1be5779150cb1c45adc2996165e
Lincoln Ombelets <lincdog85@gmail.com>
2011-09-17 03:31:00 -0400

    starting to work on navbits

--- inc/Forum.php
+++ inc/Forum.php
@@ -1,44 +1,53 @@
 <?php
-class Forum {
-	private $dbh = null;
-	private $auth = null;
-	private $tpl = null;
 
-	function __construct($dbh, $tpl) {
-		$this->dbh  = $dbh;
-		$this->auth = 1;
-		$this->tpl = $tpl;
+class Forum extends EOForum {
+	private $fid = null;
+	public $forum_data = null;
+
+	function __construct($dbh, $tpl, $fid) {
+		parent::__construct($dbh, $tpl);
+		
+		$this->fid = $fid;
+		$sql = 'SELECT * FROM forums WHERE fid = :fid';
+		$forum_query = $this->dbh->prepare($sql);
+		$forum_query->bindParam(':fid', $fid, PDO::PARAM_INT);
+		$forum_query->execute();
+		$this->forum_data = $forum_query->fetch(PDO::FETCH_ASSOC);
 	}
 
-	public function push($template, $data = null) {
-		foreach ($data as $key => $value) {
-			$this->tpl->assign($key, $value);
-		}
-		$this->tpl->display($template);
-	}
+	public function generate_navbits() {
+		$navbits = array(
 
-	public function fetch_all_forums() {
-		$forums_query = $this->dbh->prepare("SELECT * FROM forums");
-		$forums_query->execute();
-		$tForums = $forums_query->fetchAll(PDO::FETCH_ASSOC);
-		$rCats = array ();
+		);
 
-		foreach ($tForums as $forum) {
-			switch ($forum['forumtype']) {
-				case 1:
-					//Forum is a category
-					$rCats[$forum['fid']] = $forum;
-					$rCats[$forum['fid']]['children'] = array();
-					break;
-				case 0:
-					//Forum is a normal forum
-					$rCats["{$forum['parentid']}"]['children'][] = $forum['fid'];
-					$rForums[$forum['fid']] = $forum;
+		if ($this->forum_data['parentid'] === $this->forum_data['fid']) {
+			$navbits[$this->forum_data['fid']] = $this->forum_data['forumname'];
+		} else {
+			$current_fid = $this->forum_data['fid'];
+			$current_parentid = $this->forum_data['parentid'];
+
+			while (1) {
+				$sql = 'SELECT forums.fid, forums.forumname, forums.parentid FROM forums WHERE fid = :parentid';
+				$navbit_query = $this->dbh->prepare($sql);
+
+				$navbit_query->bindParam(':parentid', $current_parentid);
+				$navbit_query->execute();
+				$navbit_data = $navbit_query->fetch();
+
+				$navbits[$current_fid] = $navbit_data['forumname'];
+				
+				$current_fid = $navbit_data['fid'];
+				$current_parentid = $navbit_data['parentid'];
+
+				if ($current_fid != $this->forum_data['parentid'])
 					break;
 			}
+		
 		}
-		//pass cat. data to tpl in this form: catid => array(forumid, forumid, forumid)
-		return array($rCats, $rForums);
+		
+		$navbits[$this->forum_data['fid']] = $this->forum_data['forumname'];
+
+		return $navbits;
 	}
+
 }
-?>
